83bce9632d96689a30db080155951680c6aba8f1
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-05-31 15:55:57 +0530

    Logging Feature

--- hellowebservice/src/main/java/com/mohit/ws/soap/HelloWs.java
+++ hellowebservice/src/main/java/com/mohit/ws/soap/HelloWs.java
@@ -3,7 +3,10 @@ package com.mohit.ws.soap;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.apache.cxf.feature.Features;
+
 @WebService
+@Features(features = "org.apache.cxf.feature.LoggingFeature")
 public class HelloWs {
 	@WebMethod
 	public String hello()
